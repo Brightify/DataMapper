eaf6d0810b3a96092f5a31fe17300c2957f9bc06
Filip Dolník <filip.dolnik.96@gmail.com>
2016-12-27 13:51:31 +0100

    Add some tests for ObjectMapper.

---
+++ Tests/ObjectMapper/ObjectMapperTest.swift
@@ -0,0 +1,103 @@
+//
+//  ObjectMapperTest.swift
+//  SwiftKit
+//
+//  Created by Filip Dolnik on 24.12.16.
+//  Copyright © 2016 Brightify. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class ObjectMapperTest: QuickSpec {
+    
+    override func spec() {
+        describe("ObjectMapper") {
+            describe("serialize") {
+                context("value is Serializable") {
+                    it("works with value") {
+                        
+                    }
+                    it("works with array") {
+                        
+                    }
+                    it("works with dictionary") {
+                        
+                    }
+                    it("works with array with optionals") {
+                        
+                    }
+                    it("works with dictionary with optionals") {
+                        
+                    }
+                }
+                context("transformation provided") {
+                    it("works with value") {
+                        
+                    }
+                    it("works with array") {
+                        
+                    }
+                    it("works with dictionary") {
+                        
+                    }
+                    it("works with array with optionals") {
+                        
+                    }
+                    it("works with dictionary with optionals") {
+                        
+                    }
+                }
+            }
+            describe("deserialize") {
+                context("value is Deserializable") {
+                    it("works with value") {
+                        
+                    }
+                    it("works with array") {
+                        
+                    }
+                    it("works with dictionary") {
+                        
+                    }
+                    it("works with array with optionals") {
+                        
+                    }
+                    it("works with dictionary with optionals") {
+                        
+                    }
+                }
+                context("transformation provided") {
+                    it("works with value") {
+                        
+                    }
+                    it("works with array") {
+                        
+                    }
+                    it("works with dictionary") {
+                        
+                    }
+                    it("works with array with optionals") {
+                        
+                    }
+                    it("works with dictionary with optionals") {
+                        
+                    }
+                }
+            }
+            describe("serializableData") {
+                it("returns SerializableData") {
+                    
+                }
+            }
+            describe("deserializableData") {
+                it("returns DeserializableData") {
+                    
+                }
+            }
+            context("used polymorp") {
+                // TODO
+            }
+        }
+    }
+}
